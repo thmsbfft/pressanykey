518fa0fa02c9b75d01c80d6545a2a04f1f5bdcec
thmsbfft <thmsbfft@gmail.com>
2016-08-27 17:08:25 -0700

    Map keyboard

--- src/js/renderer/Keyboard.js
+++ src/js/renderer/Keyboard.js
@@ -7,24 +7,26 @@ function Keyboard(parameters) {
 	this.attachEvents();
 
 	this.keyboard = undefined;
+	this.keyMap = undefined;
 
 	this.debug = {
 		center: undefined
 	}
 
-	// this.resize();
 	paper.project.importSVG('../keyboard.svg', { onLoad: this.initKeyboard.bind(this) });
+	paper.view.zoom = 0.5;
+
 }
 
 Keyboard.prototype.attachEvents = function() {
 
-	window.addEventListener('keypress', function(e) {
+	window.addEventListener('keydown', function(e) {
 
-		console.log(e.keyCode);
-
+		// console.log(e.key);
+		// console.log('Loc', e.location);
 		var char = String.fromCharCode(e.keyCode);
 		
-		this.addToPath(e.key);
+		this.addToPath(e.key.toLowerCase(), e.location);
 		console.log(char, e.key);
 		e.preventDefault();
 
@@ -48,7 +50,8 @@ Keyboard.prototype.resize = function() {
 	this.debug.center.opacity = 0;
 
 	// Responsive
-	this.keyboard.setPosition(paper.view.viewSize.width/2, paper.view.viewSize.height/2);
+	this.keyboard.setPosition(paper.view.viewSize.width/1.2, paper.view.viewSize.height/2 + 100);
+	// this.keyboard.setPosition(paper.view.viewSize.width/2, paper.view.viewSize.height/2);
 
 	paper.view.draw();
 
@@ -87,13 +90,124 @@ Keyboard.prototype.initKeyboard = function(keyboard, svg) {
 
 	};
 
+	// this.keyboard.children[59].strokeColor = "red";
+
+	this.keyMap = [
+		"1",
+		"`",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+		"0",
+		"-",
+		"=",
+		"q",
+		"w",
+		"e",
+		"r",
+		"t",
+		"y",
+		"u",
+		"i",
+		"o",
+		"p",
+		"[",
+		"]",
+		"\\",
+		"a",
+		"s",
+		"d",
+		"f",
+		"g",
+		"h",
+		"j",
+		"k",
+		"l",
+		";",
+		"'",
+		"z",
+		"x",
+		"c",
+		"v",
+		"b",
+		"n",
+		"m",
+		",",
+		".",
+		"/",
+		"backspace",
+		"tab",
+		"capslock",
+		"shift", // L
+		" ", // Space
+		"//meta", // R
+		"control", // L
+		"//alt", // R
+		"alt", // L
+		"//control", // R
+		"meta", // L
+		"//shift", // R
+		"enter",
+	]
+
 	this.resize();
 
 }
 
-Keyboard.prototype.addToPath = function(key) {
+Keyboard.prototype.addToPath = function(key, location) {
 
-	// console.log(this.keyboard.children);
-	this.keyboard.strokeColor = "red";
+	console.log(location);
+
+	if(location == 1 || location == 0) {
+
+		// Left side
+
+		var index = null
+		
+		for (var i = this.keyMap.length - 1; i >= 0; i--) {
+			if(this.keyMap[i] == key) index = i;
+		}
+
+		if(index == null) {
+			console.log('No match');
+			return;
+		}
+
+		console.log(index);
+		this.keyboard.children[index].strokeColor = "red";
+
+	}
+	else if(location == 2) {
+
+		// Right side
+
+		if(key == "meta") {
+			this.keyboard.children[52].strokeColor = "blue";
+		}
+
+		if(key == "alt") {
+			this.keyboard.children[54].strokeColor = "blue";
+		}
+
+		if(key == "control") {
+			this.keyboard.children[56].strokeColor = "blue";
+		}
+
+		if(key == "shift") {
+			this.keyboard.children[58].strokeColor = "blue";
+		}
+
+	}
+
+	
+
+	// this.keyboard.strokeColor = "red";
+
+	paper.view.draw();
 
 }
