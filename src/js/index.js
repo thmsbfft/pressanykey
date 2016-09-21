2fbc18e47a2e24c181c4ced8703d460c5e4fa7be
thmsbfft <thmsbfft@gmail.com>
2016-09-21 10:56:11 -0700

    Add credits

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	console.log('👻');
+	console.log('%cP R E S S  A N Y  K E Y', 'font-style: italic');
+	console.log('Thomas Buffet');
+	console.log('2016');
+	console.log(' ');
+	console.log('%c🎨 Paper.js%c    Jürg Lehni & Jonathan Puckey • http://paperjs.org/', 'font-weight: bold', '');
+	console.log('%c🎵 Tone.js%c     Yotam Mann • https://github.com/Tonejs/Tone.js', 'font-weight: bold', '');
+	
 	App.init(document.getElementById('container'));
 
 });
